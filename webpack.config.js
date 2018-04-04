65bcae8f8f2cd3facb52c299d8637e5eafe7a177
Christopher Murphy <christopherianmurphy@gmail.com>
2018-04-03 23:06:03 -0600

    cache-control and code-splitting with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
 const path = require('path')
 const webpack = require('webpack') // eslint-disable-line
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'build.js'
+    publicPath: '/dist',
+    filename: '[name].[chunkhash].js'
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: 'initial',
+          test: path.resolve(__dirname, 'node_modules'),
+          name: 'vendor',
+          enforce: true
+        }
+      }
+    }
   },
   mode: 'development',
   module: {
@@ -26,6 +41,13 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      filename: './../_includes/scripts.html',
+      template: './_includes/_scripts.html'
+    })
+  ],
   resolve: {
     alias: {
       vue$: 'vue/dist/vue.esm.js'
